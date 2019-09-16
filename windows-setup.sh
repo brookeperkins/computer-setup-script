d5d2f7be516117238c7f8db645c5d018c6b43eb3
John Cokos <highlander.44@gmail.com>
2019-09-15 21:59:30 -0700

    Added a bit of ux to it

--- windows-setup.sh
+++ windows-setup.sh
@@ -1,52 +1,69 @@
 #!/bin/bash
 
 ## Install our alias files and initialize them
+clear
+echo "Installing Core WSL Aliases and Helpers"
 curl https://raw.githubusercontent.com/codefellows/computer-setup/master/.wsl --output .wsl
 curl https://raw.githubusercontent.com/codefellows/computer-setup/master/.gitprompt --output .gitprompt
+echo "source ~/.gitprompt" >> ~/.bashrc
+echo "source ~/.wsl" >> ~/.bashrc
 source ~/.wsl
 source ~/.gitprompt
 
-echo "source ~/.gitprompt" >> ~/.bashrc
-echo "source ~/.wsl" >> ~/.bashrc
 
-## Update APT
+clear
+echo "Updating APT Database"
 sudo apt get update
 
-# Install Git
+clear
+echo "Installing 'git'"
 sudo apt install git
+## TODO: Setup the git configuration to use SSH
 
+clear
+echo "Installing the 'tree' utility"
 # Install Tree
 sudo apt install tree
 
-# Install NPM and Node
+clear
+echo "Installing 'node' with 'nvm'"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 nvm use stable
 
-# Install some global node apps
+clear
+echo "Installing helper applictions 'nodemon, live-server, json-server'"
 npm install -g nodemon
 npm install -g live-server
 npm install -g json-server
 
-# Heroku
+clear
+echo "Installing 'heroku'"
 bash <(curl -s https://cli-assets.heroku.com/install.sh)
 
-# Postgres
+clear
+echo "Installing 'postgres database server and client'"
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install postgresql-10
-sudo service postgresql start
-# Auto Start?
+## TODO: Auto Start?
 
-echo "Create Postgres user"
+clear
+echo "Create Postgres user and default database"
+echo "   When Prompted ..."
+echo "     - Use your wsl username as the database name"
+echo "     - Provide your wsl username and password to create a new pg user"
+echo "     - Say 'Yes' when asked if this user should be a Super User'"
+echo ""
 sudo -u postgres createuser --interactive --pwprompt
 sudo -u postgres createdb -O `whoami` `whoami`
 
-# Mongo
+clear
+echo "Installing 'mongo database server and client'"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo mkdir -p ~/data/db
-sudo mongod --dbpath ~/data/db
-# Auto Start?
+## TODO:  Auto Start?
 
+clear
 verify
