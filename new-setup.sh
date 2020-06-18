988b3f2b16e2bd61264b4151968bd48fd9746cb7
David Goodwin <david@alchemycodelab.com>
2020-06-17 20:33:17 -0700

    updated mongo-redhat install script

--- new-setup.sh
+++ new-setup.sh
@@ -70,6 +70,7 @@ install-mongo-redhat() {
   # TODO
   # Change this to the git location for the .repo file
   sudo curl -o- https://raw.githubusercontent.com/alchemycodelab/computer-setup-script/script-rewrite/lib/mongodb-org-4.2.repo > /etc/yum.repos.d/mongodb-org-4.2.repo
+
   sudo yum install -y mongodb-org
   # installing MongoBD Compass, as well
   curl -o- https://downloads.mongodb.com/compass/mongodb-compass-1.21.2.x86_64.rpm > ~/.alchemy/downloads/mongodb-compass-1.21.2.x86_64.rpm
