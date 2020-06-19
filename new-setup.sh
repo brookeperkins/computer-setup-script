76d2a698636e7097a183a8411def026a14b59327
David Goodwin <david@alchemycodelab.com>
2020-06-19 12:09:47 -0700

    missing sudo

--- new-setup.sh
+++ new-setup.sh
@@ -51,7 +51,7 @@ install-mongo-debian() {
   curl -qo- https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
   echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
   $apt update
-  apt install mongodb-org
+  sudo apt install mongodb-org
 
   curl -qo- https://downloads.mongodb.com/compass/mongodb-compass-community_1.21.2_amd64.deb > ~/.alchemy/downloads/mongodb-compass-community_1.21.2_amd64.deb
   sudo dpkg -i ~/.alchemy/downloads/mongodb-compass-community_1.21.2_amd64.deb
